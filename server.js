f44d61f19847a84d89d455a6b310e1ec5c8bd577
Andela Culjak <andela212@gmail.com>
2019-10-21 16:01:38 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API running"));
+const PORT = 5010;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
