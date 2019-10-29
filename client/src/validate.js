e56bd2ceebf29fcbdfc7c3724934f79839b1f35f
Andela Culjak <andela212@gmail.com>
2019-10-29 01:26:55 +0100

    Final commit

--- client/src/validate.js
+++ client/src/validate.js
@@ -17,10 +17,8 @@ export const validate = ({
     termsError: ""
   };
 
-  //allows numbers without national code eg. +000
   //const regexPhone = /(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s]?[(]?[0-9]{1,3}[)]?([-\s]?[0-9]{3})([-\s]?[0-9]{3,4})/;
 
-  // checks if form of +0000000000, accepts whitespaces
   const regexPhone = /^\+[\d\s]{8,25}$/gi;
   const regexEmail = /^(([^<>()\],;:\s@]+(\.[^<>()\],;:\s@]+)*)|(.+))@(([^<>()[\],;:\s@]+\.)+[^<>()[\],;:\s@]{2,})$/i;
 
